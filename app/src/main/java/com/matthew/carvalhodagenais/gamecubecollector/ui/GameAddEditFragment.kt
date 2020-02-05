136bb6007c7dbb80c9a718fe355fcb1a8b0cbe1f
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-02-04 21:01:12 -0500

    Code cleanup

--- app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/ui/GameAddEditFragment.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/ui/GameAddEditFragment.kt
@@ -2,6 +2,7 @@ package com.matthew.carvalhodagenais.gamecubecollector.ui
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -18,7 +19,7 @@ class GameAddEditFragment: Fragment() {
     private var isFav: Boolean = false
 
     private var releaseDateToSave: Calendar? = null
-    private var buyDate: Calendar? = null
+    private var buyDateToSave: Calendar? = null
 
     companion object {
 
@@ -52,21 +53,21 @@ class GameAddEditFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val request = arguments!!.getInt(REQUEST_CODE)
 
         favourite_image_button.setOnClickListener(favouriteButtonOnClick)
+        val game: Game = viewModel.getSelectedGame()!!
 
-        if (request == EDIT_REQUEST) { // Set all values in the UI
-            setFavouriteStarDrawable(viewModel.getSelectedGame()?.isFavourite ?: false)
-            title_edit_text.setText(viewModel.getSelectedGame()!!.title)
-            developer_edit_text.setText(viewModel.getSelectedGame()?.developers)
-            publisher_edit_text.setText(viewModel.getSelectedGame()?.publishers)
-            price_paid_edit_text.setText(viewModel.getSelectedGame()?.pricePaid?.toString() ?: "")
-            case_checkbox.isChecked = viewModel.getSelectedGame()?.hasCase ?: false
-            manual_checkbox.isChecked = viewModel.getSelectedGame()?.hasManual ?: false
+        // Set all values in the UI
+        if (arguments!!.getInt(REQUEST_CODE) == EDIT_REQUEST) {
+            setUIValues(game)
+
+            Log.e("RELEASE", game.releaseDate?.time.toString())
+            Log.e("BUY", game.boughtDate?.time.toString())
         }
 
+        // Set OnClickListeners for the "pick date" anc "clear date" buttons
         release_date_calendar_image_button.setOnClickListener(releaseDateOnClickListener)
+        buy_date_calendar_image_button.setOnClickListener(buyDateOnClickListener)
     }
 
     override fun onPrepareOptionsMenu(menu: Menu) {
@@ -99,6 +100,23 @@ class GameAddEditFragment: Fragment() {
     }
 
     /**
+     * Sets all UI values from game data
+     *
+     * @param game Game
+     */
+    private fun setUIValues(game: Game) {
+        setFavouriteStarDrawable(game.isFavourite ?: false)
+        title_edit_text.setText(game.title)
+        developer_edit_text.setText(game.developers)
+        publisher_edit_text.setText(game.publishers)
+        price_paid_edit_text.setText(game.pricePaid?.toString() ?: "")
+        case_checkbox.isChecked = game.hasCase ?: false
+        manual_checkbox.isChecked = game.hasManual ?: false
+        release_date_edit_text.setText(createDateString(game.releaseDate))
+        buy_date_edit_text.setText(createDateString(game.boughtDate))
+    }
+
+    /**
      * Saves the game to the database by grabbing all values from the inputs
      */
     private fun saveGame() {
@@ -121,6 +139,7 @@ class GameAddEditFragment: Fragment() {
             developers = setNullIfEmptyString(developer_edit_text.text.toString())
             releaseDate = releaseDateToSave?.time
             pricePaid = setNullIfEmptyString(price_paid_edit_text.text.toString())?.toDouble()
+            boughtDate = buyDateToSave?.time
             hasCase = case_checkbox.isChecked
             hasManual = manual_checkbox.isChecked
         }
@@ -134,9 +153,37 @@ class GameAddEditFragment: Fragment() {
         }
     }
 
+    /**
+     * Creates a formatted date string from a Date
+     *
+     * @param date Date
+     * @return String
+     */
+    private fun createDateString(date: Date?): String {
+        var str = ""
+        if (date != null) {
+            val cal = Calendar.getInstance()
+            cal.time = Date(date.time)
+            str = "${cal.get(Calendar.DAY_OF_MONTH)}/" +
+                    "${cal.get(Calendar.MONTH)}/" +
+                    "${cal.get(Calendar.YEAR)}"
+        } else {
+            str = getString(R.string.date_default)
+        }
+        return str
+    }
+
+    /**
+     * Sets a String to null if it is empty
+     */
     private fun setNullIfEmptyString(string: String): String? =
         if (string.trim().isEmpty()) null else string
 
+    /**
+     * Changes the star drawable to filled or bordered
+     *
+     * @param isFav bool
+     */
     private fun setFavouriteStarDrawable(isFav: Boolean) {
         if (isFav) {
             favourite_image_button.setImageDrawable(
@@ -163,20 +210,41 @@ class GameAddEditFragment: Fragment() {
         return cal
     }
 
+    /**
+     * OnClickListener for the favourite button
+     * Sets the boolean for "favourited" and changes the star drawable
+     */
     private val favouriteButtonOnClick = View.OnClickListener {
         isFav = !isFav
         setFavouriteStarDrawable(isFav)
     }
 
     /**
-     * OnClickListener to set a birthday
+     * OnClickListener to set a date
+     * Sets the release date on click
      */
     private val releaseDateOnClickListener = View.OnClickListener {
         val cal = Calendar.getInstance()
         val datePicker = DatePickerDialog(context!!,
             DatePickerDialog.OnDateSetListener{_, mYear, mMonth, mDay ->
-                Toast.makeText(context!!, "${mYear}/${mMonth + 1}/${mDay}", Toast.LENGTH_SHORT).show()
+                release_date_edit_text.setText("${mDay}/${mMonth + 1}/${mYear}")
                 releaseDateToSave = createCalendar(mYear, mMonth + 1, mDay)
+            }, cal.get(Calendar.YEAR),
+            cal.get(Calendar.MONTH),
+            cal.get(Calendar.DAY_OF_MONTH))
+        datePicker.show()
+    }
+
+    /**
+     * OnClickListener to set a date
+     * Sets the buy date on click
+     */
+    private val buyDateOnClickListener = View.OnClickListener {
+        val cal = Calendar.getInstance()
+        val datePicker = DatePickerDialog(context!!,
+            DatePickerDialog.OnDateSetListener{_, mYear, mMonth, mDay ->
+                buy_date_edit_text.setText("${mDay}/${mMonth + 1}/${mYear}")
+                buyDateToSave = createCalendar(mYear, mMonth + 1, mDay)
             }, cal.get(Calendar.YEAR),
             cal.get(Calendar.MONTH),
             cal.get(Calendar.DAY_OF_MONTH))
