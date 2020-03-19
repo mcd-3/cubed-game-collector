1dfeb1f4a5d727712defb698e8fdd118ce204225
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-03-18 20:09:33 -0400

    Added completeness text

--- app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/databinders/TextViewDataBinder.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/databinders/TextViewDataBinder.kt
@@ -1,9 +1,14 @@
 package com.matthew.carvalhodagenais.gamecubecollector.databinders
 
+import android.content.res.Resources
+import android.content.res.TypedArray
+import android.graphics.Typeface
+import android.util.TypedValue
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import com.google.android.material.internal.ContextUtils
 import com.matthew.carvalhodagenais.gamecubecollector.R
 import com.matthew.carvalhodagenais.gamecubecollector.data.repositories.ConditionRepository
 import com.matthew.carvalhodagenais.gamecubecollector.data.repositories.RegionRepository
@@ -32,6 +37,34 @@ class TextViewDataBinder {
             } else {
                 textView.text = textView.context.getString(R.string.no_condition)
             }
+        }
+
+        @JvmStatic
+        @BindingAdapter("bind:isDefault")
+        fun setDefaultTextStyle(textView: TextView, isDefault: Boolean) {
+            if (isDefault) {
+                textView.setTypeface(null, Typeface.ITALIC)
+
+                // get theme subtitle color
+//                val typedValue = TypedValue()
+//                val theme: Resources.Theme = textView.context.applicationContext.theme
+//                theme.resolveAttribute(R.attr.subtitleTextColor, typedValue, true)
+//                textView.setTextColor(typedValue.data)
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("bind:hasCase", "bind:hasManual")
+        fun setCompletenessText(textView: TextView, hasCase: Boolean, hasManual: Boolean) {
+            var str = textView.context.getString(R.string.completeness_disc);
+            if (hasCase && hasManual) {
+                str = textView.context.getString(R.string.completeness_cib);
+            } else if (hasCase) {
+                str = textView.context.getString(R.string.completeness_case);
+            } else if (hasManual) {
+                str = textView.context.getString(R.string.completeness_manual);
+            }
+            textView.text = str
         }
     }
 }
