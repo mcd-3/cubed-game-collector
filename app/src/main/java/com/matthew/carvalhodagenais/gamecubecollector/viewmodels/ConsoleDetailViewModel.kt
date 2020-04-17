3912c3b7baf5af388a8c9a1a1f75de30dfb94977
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-04-16 22:52:12 -0400

    Added functions to ConsoleDetailViewModel

--- app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/viewmodels/ConsoleDetailViewModel.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/viewmodels/ConsoleDetailViewModel.kt
@@ -2,8 +2,33 @@ package com.matthew.carvalhodagenais.gamecubecollector.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.matthew.carvalhodagenais.gamecubecollector.data.entities.Console
+import com.matthew.carvalhodagenais.gamecubecollector.data.repositories.ConsoleRepository
+import kotlinx.coroutines.launch
 
 class ConsoleDetailViewModel(application: Application): AndroidViewModel(application) {
 
+    private var selectedConsole = MutableLiveData<Console>()
+    private var repository = ConsoleRepository(application)
 
+    fun insert(console: Console) = viewModelScope.launch {
+        repository.insertConsole(console)
+    }
+
+    fun update(console: Console) = viewModelScope.launch {
+        repository.updateConsole(console)
+    }
+
+    fun delete(console: Console) = viewModelScope.launch {
+        repository.deleteConsole(console)
+    }
+
+    fun setSelectedConsole(console: Console) {
+        selectedConsole.value = console
+    }
+
+    fun getSelectedConsole(): Console? = selectedConsole.value
 }
