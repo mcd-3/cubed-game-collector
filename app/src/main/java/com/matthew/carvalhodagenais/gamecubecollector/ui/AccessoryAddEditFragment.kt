2846c96a4ec65fecb06d8d3a134d0fc76fbb7a6d
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-05-03 23:14:26 -0400

    Saves text for EditTexts on orientation change

--- app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/ui/AccessoryAddEditFragment.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/gamecubecollector/ui/AccessoryAddEditFragment.kt
@@ -6,10 +6,12 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
 import com.matthew.carvalhodagenais.gamecubecollector.MainActivity
 import com.matthew.carvalhodagenais.gamecubecollector.R
 import com.matthew.carvalhodagenais.gamecubecollector.databinding.FragmentAccessoryAddEditBinding
+import com.matthew.carvalhodagenais.gamecubecollector.helpers.ImageStorageHelper
 import com.matthew.carvalhodagenais.gamecubecollector.helpers.generators.TextWatcherGenerator
 import com.matthew.carvalhodagenais.gamecubecollector.viewmodels.AccessoryAddEditViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,6 +23,9 @@ class AccessoryAddEditFragment: Fragment() {
 
     companion object {
         private const val REQUEST_CODE: String = "AccessoryAddEditFragment.REQUEST_CODE"
+        private const val TITLE_KEY = "TITLE"
+        private const val DESCRIPTION_KEY = "DESC"
+        private const val CONDITION_KEY = "COND"
         const val ADD_REQUEST = 1
         const val EDIT_REQUEST = 2
     }
@@ -41,6 +46,7 @@ class AccessoryAddEditFragment: Fragment() {
             this.textWatcher = tw.generate(addEditViewModel)
         }
         setHasOptionsMenu(true)
+        binding.executePendingBindings()
         return binding.root
     }
 
@@ -48,6 +54,22 @@ class AccessoryAddEditFragment: Fragment() {
         menu.clear()
         requireActivity().menuInflater.inflate(R.menu.menu_item_add_edit, menu)
         super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(TITLE_KEY, title_edit_text.text.toString())
+        outState.putString(DESCRIPTION_KEY, description_edit_text.text.toString())
+        outState.putInt(CONDITION_KEY, condition_spinner.selectedItemPosition)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        if (savedInstanceState != null) {
+            title_edit_text.setText(savedInstanceState.getString(TITLE_KEY))
+            description_edit_text.setText(savedInstanceState.getString(DESCRIPTION_KEY))
+            condition_spinner.setSelection(savedInstanceState.getInt(CONDITION_KEY))
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
