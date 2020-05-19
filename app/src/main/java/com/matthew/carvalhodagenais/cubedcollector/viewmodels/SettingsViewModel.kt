18ce0ee4f2b1ef6a75ed4779c10f461ac2e2ebd5
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2020-05-19 18:18:39 -0400

    Fixed accessory deletion + small modifications

--- app/src/main/java/com/matthew/carvalhodagenais/cubedcollector/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/com/matthew/carvalhodagenais/cubedcollector/viewmodels/SettingsViewModel.kt
@@ -3,6 +3,7 @@ package com.matthew.carvalhodagenais.cubedcollector.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.matthew.carvalhodagenais.cubedcollector.data.repositories.AccessoryRepository
 import com.matthew.carvalhodagenais.cubedcollector.data.repositories.ConsoleRepository
 import com.matthew.carvalhodagenais.cubedcollector.data.repositories.GameRepository
 import com.matthew.carvalhodagenais.cubedcollector.helpers.ImageStorageHelper
@@ -10,13 +11,15 @@ import kotlinx.coroutines.launch
 
 class SettingsViewModel(application: Application): AndroidViewModel(application) {
 
-    val gameRepo: GameRepository = GameRepository(application)
-    val consoleRepo: ConsoleRepository = ConsoleRepository(application)
+    private val gameRepo: GameRepository = GameRepository(application)
+    private val consoleRepo: ConsoleRepository = ConsoleRepository(application)
+    private val accessoryRepo: AccessoryRepository = AccessoryRepository(application)
 
 
     fun deleteAllData() = viewModelScope.launch {
         gameRepo.deleteAllGames()
         consoleRepo.deleteAllConsoles()
+        accessoryRepo.deleteAllAccessories()
         ImageStorageHelper.deleteAllImages()
     }
 
